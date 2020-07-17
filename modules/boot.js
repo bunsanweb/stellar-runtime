ad56f66f7d7b0f87e4bf47b0aabac504be0ca76b
bellbind <bellbind@gmail.com>
2020-07-17 17:25:58 +0900

    [update] save sandbox persistency for reboot

--- modules/boot.js
+++ modules/boot.js
@@ -1,11 +1,12 @@
 // TBD: temporary UI
 
 import {Sandbox} from "./sandbox.js";
+import {Storage} from "./storage.js";
 
 const win = nw.Window.get();
 win.showDevTools();
 
-const sandboxView = sandbox => {
+const createSandboxView = (sandbox, url = "") => {
   const view = document.createElement("div");
   const inputs = document.createElement("div");
 
@@ -13,34 +14,66 @@ const sandboxView = sandbox => {
   configInput.value = JSON.stringify(sandbox.config);
   
   const urlInput = document.createElement("input");
+  urlInput.value = url;
   const loadButton = document.createElement("button");
   loadButton.addEventListener("click", ev => {
     (async () => {
-      //try {
+      try {
         const config = JSON.parse(configInput.value);
         sandbox.update(config);
-      //} catch {}
+      } catch (error) {}
       await sandbox.restart(urlInput.value.trim());
       sandbox.iframe.style.display = "block";
     })();
   });
   loadButton.append("load");
 
-  
   inputs.append(urlInput, loadButton, configInput);
   view.append(inputs, sandbox.iframe);
   return view;
 };
 
-const sandboxes = [];
-const sandboxList = document.createElement("div");
-const newButton = document.createElement("button");
-newButton.addEventListener("click", ev => {
-  const sandbox = Sandbox.create();
-  const view = sandboxView(sandbox);
-  sandboxList.append(view);
-});
-newButton.append("new sandbox");
+const main = async () => {
+  const storage = await Storage.open();
 
+  // load from storage
+  const sandboxes = [];
+  const sandboxPromises = [];
+  const sandboxList = document.createElement("div");
+  for (const {id, config = {}, url = ""} of await storage.getAllSandboxes()) {
+    console.log(id, config, url);
+    const sandbox = new Sandbox(id, config);
+    sandbox.addEventListener("updated", ev => {
+      (async () => {
+        await storage.putSandbox(sandbox.data);
+      })().catch(console.error);
+    });
+    const sandboxPromise = sandbox.start(url);
+    const sandboxView = createSandboxView(sandbox, url);
+    sandboxes.push(sandbox);
+    sandboxPromises.push(sandboxPromise);
+    sandboxList.append(sandboxView);
+  }
+  
+  // new sandbox button
+  const newButton = document.createElement("button");
+  newButton.addEventListener("click", ev => {
+    const sandbox = Sandbox.create();
+    (async () => {
+      await storage.postSandbox(sandbox.data);
+    })().catch(console.error);
+    sandbox.addEventListener("updated", ev => {
+      (async () => {
+        await storage.putSandbox(sandbox.data);
+      })().catch(console.error);
+    });
+    const view = createSandboxView(sandbox);
+    sandboxList.append(view);
+  });
+  newButton.append("new sandbox");
+  
+  document.body.append(newButton, sandboxList);
+  await Promise.all(sandboxPromises);
+};
 
-document.body.append(newButton, sandboxList);
+main().catch(console.error);
